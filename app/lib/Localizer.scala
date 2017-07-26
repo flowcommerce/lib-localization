6821024409d0421b92399866ab0304d13594b08b
Michael Bryzek <mbryzek@alum.mit.edu>
2017-07-26 15:01:29 +0100

    WIP

--- app/lib/Localizer.scala
+++ app/lib/Localizer.scala
@@ -12,25 +12,62 @@ import scala.concurrent.{ExecutionContext, Future}
 trait Localizer {
 
   /**
-    * Returns the localized pricing of the specified item for the specified country
+    * Returns the localized pricing of the specified item for the specified country, using
+    * the default currency for that country.
+    * 
     * @param country country in the ISO 3166-3 format
-    * @param itemId the id of the item
+    * @param itemNumber the id of the item
     * @return the localized pricing of the specified item for the specified country
     */
-  def getByCountry(country: String, itemId: String)(
+  def getByCountry(country: String, itemNumber: String)(
     implicit executionContext: ExecutionContext
   ): Future[Option[LocalizedPricing]]
 
   /**
+    * Returns the localized pricing of the specified item for the specified country,
+    * then converting as necessary to the specified target currency.
+    * 
+    * @param country country in the ISO 3166-3 format
+    * @param itemNumber the id of the item
+    * @param targetCurrency the ISO currency code
+    * @return the localized pricing of the specified item for the specified country
+    */
+  def getByCountry(country: String, itemNumber: String, targetCurrency: String)(
+    implicit executionContext: ExecutionContext
+  ): Future[Option[LocalizedPricing]] = {
+    getByCountry(country, itemNumber).map( _.map(convert(_, targetCurrency)) )
+  }
+
+  /**
+    * Converts a given price to the specified target currency
+    *
+    * @param targetCurrency the ISO currency code
+    */
+  def convert(pricing: LocalizedPricing, targetCurrency: String): LocalizedPricing
+
+  /**
     * Returns localized pricing of the specified item for the specified experience
-    * @param experienceId the id of the experience
-    * @param itemId the id of the item
+    * @param experienceKey the id of the experience
+    * @param itemNumber the id of the item
     * @return the localized pricing of the specified item for the specified experience
     */
-  def getByExperience(experienceId: String, itemId: String)(
+  def getByExperience(experienceKey: String, itemNumber: String)(
     implicit executionContext: ExecutionContext
   ): Future[Option[LocalizedPricing]]
 
+
+  /**
+    * Returns localized pricing of the specified item for the specified experience
+    * @param experienceKey the id of the experience
+    * @param itemNumber the id of the item
+    * @param targetCurrency the ISO currency code
+    * @return the localized pricing of the specified item for the specified experience
+    */
+  def getByExperience(experienceKey: String, itemNumber: String, targetCurrency: String)(
+    implicit executionContext: ExecutionContext
+  ): Future[Option[LocalizedPricing]] = {
+    getByExperience(experienceKey, itemNumber).map( _.map(convert(_, targetCurrency)) )
+  }
 }
 
 object Localizer {
@@ -50,16 +87,16 @@ object Localizer {
 
 class LocalizerImpl @Inject() (localizerClient: LocalizerClient) extends Localizer {
 
-  override def getByCountry(country: String, itemId: String)(
+  override def getByCountry(country: String, itemNumber: String)(
     implicit executionContext: ExecutionContext
   ): Future[Option[LocalizedPricing]] = {
-    get(CountryKey(country = country, itemId = itemId))
+    get(CountryKey(country = country, itemNumber = itemNumber))
   }
 
-  override def getByExperience(experienceId: String, itemId: String)(
+  override def getByExperience(experienceKey: String, itemNumber: String)(
     implicit executionContext: ExecutionContext
   ): Future[Option[LocalizedPricing]] = {
-    get(ExperienceKey(experience = experienceId, itemId = itemId))
+    get(ExperienceKey(experience = experienceKey, itemNumber = itemNumber))
   }
 
   private def get(keyProvider: KeyProvider)(
@@ -78,10 +115,10 @@ private[this] trait KeyProvider {
   def getKey: String
 }
 
-private[this] case class CountryKey(country: String, itemId: String) extends KeyProvider {
-  def getKey: String = s"country-$country:$itemId"
+private[this] case class CountryKey(country: String, itemNumber: String) extends KeyProvider {
+  def getKey: String = s"country-$country:$itemNumber"
 }
 
-private[this] case class ExperienceKey(experience: String, itemId: String) extends KeyProvider {
-  def getKey: String = s"experience-$experience:$itemId"
+private[this] case class ExperienceKey(experience: String, itemNumber: String) extends KeyProvider {
+  def getKey: String = s"experience-$experience:$itemNumber"
 }
