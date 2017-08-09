1c97648a05587bce5ab7f1ed28c5943e6318e9c7
Jean Logeart <jean.logeart@gmail.com>
2017-08-09 13:43:54 -0400

    Rename ScalaFuture

--- src/main/scala/io/flow/localization/LocalizerClient.scala
+++ src/main/scala/io/flow/localization/LocalizerClient.scala
@@ -6,19 +6,19 @@ import com.twitter.finagle.redis
 import com.twitter.finagle.redis.util._
 import com.twitter.util.{Return, Throw, Future => TwitterFuture}
 
-import scala.concurrent.{ExecutionContext, Future => ScalaFuture, Promise => ScalaPromise}
+import scala.concurrent.{ExecutionContext, Future, Promise => ScalaPromise}
 
 trait LocalizerClient {
 
   /**
     * Returns the value associated with the specified key, if any
     */
-  def get(key: String)(implicit ec: ExecutionContext): ScalaFuture[Option[String]]
+  def get(key: String)(implicit ec: ExecutionContext): Future[Option[String]]
 
   /**
     * Returns the values associated with the specified keys, if any
     */
-  def mGet(keys: Seq[String])(implicit ec: ExecutionContext): ScalaFuture[Seq[Option[String]]]
+  def mGet(keys: Seq[String])(implicit ec: ExecutionContext): Future[Seq[Option[String]]]
 
 }
 
@@ -26,11 +26,11 @@ class RedisLocalizerClient @Inject() (redisClient: redis.Client) extends Localiz
 
   import RedisLocalizerClient._
 
-  override def get(key: String)(implicit ec: ExecutionContext): ScalaFuture[Option[String]] = {
+  override def get(key: String)(implicit ec: ExecutionContext): Future[Option[String]] = {
     redisClient.get(StringToBuf(key)).asScala.map(_.map(BufToString.apply))
   }
 
-  override def mGet(keys: Seq[String])(implicit ec: ExecutionContext): ScalaFuture[Seq[Option[String]]] = {
+  override def mGet(keys: Seq[String])(implicit ec: ExecutionContext): Future[Seq[Option[String]]] = {
     redisClient.mGet(keys.map(StringToBuf.apply)).asScala.map(_.map(_.map(BufToString.apply)))
   }
 
@@ -40,7 +40,7 @@ object RedisLocalizerClient {
 
   // as defined in https://twitter.github.io/util/guide/util-cookbook/futures.html
   private implicit class RichTwitterFuture[A](val tf: TwitterFuture[A]) extends AnyVal {
-    def asScala(implicit e: ExecutionContext): ScalaFuture[A] = {
+    def asScala(implicit e: ExecutionContext): Future[A] = {
       val promise: ScalaPromise[A] = ScalaPromise()
       tf.respond {
         case Return(value) => promise.success(value)
