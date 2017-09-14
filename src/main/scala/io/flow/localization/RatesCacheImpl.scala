2a2c7f80cb56cf9a5127608cbf3e4b502793b361
Michael Bryzek <mbryzek@flow.io>
2017-09-14 11:48:00 +0100

    cleanup

--- src/main/scala/io/flow/localization/RatesCacheImpl.scala
+++ src/main/scala/io/flow/localization/RatesCacheImpl.scala
@@ -4,7 +4,6 @@ import io.flow.localization.RatesCacheImpl.RateKey
 import io.flow.published.event.v0.models.json._
 import io.flow.published.event.v0.models.{OrganizationRatesData => Rates}
 import io.flow.reference.{Currencies, data}
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import play.api.libs.json.Json
 
