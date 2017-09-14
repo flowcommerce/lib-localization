d12d9d521edee08ea2de46bfb5053ee067532940
Eric Mittelhammer <code@ericmittelhammer.com>
2017-09-14 14:14:45 +0100

    recover from redis errors (#29)
    * recover from redis errors
    
    * semver

--- build.sbt
+++ build.sbt
@@ -40,4 +40,4 @@ publishTo := {
     Some("Artifactory Realm" at s"$host/libs-release-local")
   }
 }
-version := "0.0.22"
+version := "0.0.23"
--- src/main/scala/io/flow/localization/LocalizerClient.scala
+++ src/main/scala/io/flow/localization/LocalizerClient.scala
@@ -6,6 +6,7 @@ import javax.inject.Inject
 import com.twitter.finagle.redis
 import com.twitter.util.{Return, Throw, Future => TwitterFuture}
 import org.jboss.netty.buffer.ChannelBuffers
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.{ExecutionContext, Future, Promise => ScalaPromise}
 
@@ -25,16 +26,30 @@ trait LocalizerClient {
 
 class RedisLocalizerClient @Inject() (redisClient: redis.Client) extends LocalizerClient {
 
+  private val log = LoggerFactory.getLogger(getClass)
+
   import StandardCharsets._
 
   import RedisLocalizerClient._
 
   override def get(key: String)(implicit ec: ExecutionContext): Future[Option[String]] = {
     redisClient.get(ChannelBuffers.copiedBuffer(key, UTF_8)).asScala.map(_.map(_.toString(UTF_8)))
+      .recover {
+        case ex: Throwable => {
+          log.warn("FlowError - failed to get key ${key} from redis cache. ${ex.getMessage}", ex)
+          None
+        }
+      }
   }
 
   override def mGet(keys: Seq[String])(implicit ec: ExecutionContext): Future[Seq[Option[String]]] = {
     redisClient.mGet(keys.map(ChannelBuffers.copiedBuffer(_, UTF_8))).asScala.map(_.map(_.map(_.toString(UTF_8))))
+      .recover {
+        case ex: Throwable => {
+          log.warn("FlowError - failed to mget keys ${keys} from redis cache. ${ex.getMessage}", ex)
+          Nil
+        }
+      }
   }
 
 }
